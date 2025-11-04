2d683d0f52fdcb3829fd40e2af6d79461947e9a5
Ilan Zini <ilan.zini@thalesgroup.com>
2025-11-04 08:56:20 +0000

    inference and train on yolo model

--- inference.py
+++ inference.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+
+# Display model information
+model.info()
 
 # Train the model
-results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
+#results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
 
 # Run inference on an image
 #results = model("images/img3.jpg", show=True, save=True) # return a Result object
