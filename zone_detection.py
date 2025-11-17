35054e2c3ff6c0420ef505912f9ebafed145c986
Ilan Zini <ilan.zini@thalesgroup.com>
2025-11-17 09:28:57 +0000

    zone detection function done

--- zone_detection.py
+++ zone_detection.py
@@ -1,17 +1,17 @@
 from numpy.typing import NDArray
 from typing import Dict, List
+from ultralytics import YOLO
 
 
 class ZoneDetection:
     def __init__(self):
         # Load model from pt
-        self.model = ...
+        self.model = YOLO("models/yolo11n_D-Fire.pt")
 
-    def get_zones(self, img:NDArray) -> List[Dict]:
+    def get_zones(self, img) -> List[Dict]:
         """
         Params:
-        img: NDArray
-            Input image as a numpy array.
+        img: Input image as a JPG image
 
         Returns:
         List[Dict]
@@ -22,7 +22,25 @@ class ZoneDetection:
         """
         # Run inference
         results = self.model(img)
+        #results = self.model(img, save=True) # if we wanna see the result image with the detected boxes
 
         # Parse results
-        parsed_results = ...
+        parsed_results = []
+        r = results[0] # YOLO returns a list → we take the first (and only) result
+        for box in r.boxes: # Iterate over detected boxes
+            cls_id = int(box.cls[0])           # numeric class ID
+            cls_name = r.names[cls_id]         # class name ("fire"/"smoke")
+            conf = float(box.conf[0])          # confidence score
+            xyxy = box.xyxy[0].tolist()        # [x1, y1, x2, y2]
+
+            parsed_results.append({
+                "type": cls_name,
+                "bbox": [float(x) for x in xyxy],
+                "score": conf
+            })
         return parsed_results
+
+
+image = "D-Fire_dataset/test/images/WEB10490.jpg" # example
+zone_detection = ZoneDetection()
+zone_detection.get_zones(image)
