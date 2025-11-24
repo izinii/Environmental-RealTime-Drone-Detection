4e790084af9a6d95130b59e72f4d9169702d04ac
Ilan Zini <ilan.zini@thalesgroup.com>
2025-11-24 15:55:45 +0000

    cleaning

--- segmentation_image.py
+++ segmentation_image.py
@@ -1,7 +1,6 @@
 import cv2
-from tensorflow.keras.models import load_model
 import numpy as np
-import os
+from tensorflow.keras.models import load_model
 import time
 
 model_smoke = load_model("models/model_smoke_big.hdf5")
@@ -12,11 +11,6 @@ smoke_threshold = 0
 image = cv2.imread("segmentation_data/photo_test.jpg")         
 img_size = (image.shape[1],image.shape[0])
 out = image.copy()
-
-if not os.path.exists("/datas02/t0323469/hackathon/output_image_segmentation/"):
-    os.mkdir("/datas02/t0323469/hackathon/output_image_segmentation/")
-if not os.path.exists("/datas02/t0323469/hackathon/output_mask/"):
-    os.mkdir("/datas02/t0323469/hackathon/output_mask/")
 
 def do_inference(image_,model):
     #preprocess the image and run the inference
@@ -31,24 +25,14 @@ def do_inference(image_,model):
 #fire
 pred = do_inference(image,model_fire)
 pred[pred<fire_threshold]=0
-threshold_value = 0 # Choisir un seuil de 0.5 ou tout autre valeur
-binary_mask = np.where(pred >= threshold_value, 255, 0).astype(np.uint8)  # Applique le seuil et convertit en 0 ou 255
-cv2.imwrite("/datas02/t0323469/hackathon/output_mask/mask_fire_" + str(time.time()).replace(".", "") + ".png", binary_mask)
-probability_mask = (pred * 255).astype(np.uint8)  # Multiplie par 255 et convertit en uint8
-cv2.imwrite("/datas02/t0323469/hackathon/output_mask/probability_" + str(time.time()).replace(".", "") + ".png", probability_mask)
 out[:, :, 1] = cv2.addWeighted(out[:, :, 1].astype('uint8'), 1.0,
                             cv2.resize((pred * 255).astype('uint8'), img_size).astype('uint8'), 1.0, 0)
 
 #smoke
 pred = do_inference(image,model_smoke)
 pred[pred<smoke_threshold]=0
-threshold_value = 0  # Choisir un seuil de 0.5 ou tout autre valeur
-binary_mask = np.where(pred >= threshold_value, 255, 0).astype(np.uint8)  # Applique le seuil et convertit en 0 ou 255
-cv2.imwrite("/datas02/t0323469/hackathon/output_mask/mask_fire_" + str(time.time()).replace(".", "") + ".png", binary_mask)
-probability_mask = (pred * 255).astype(np.uint8)  # Multiplie par 255 et convertit en uint8
-cv2.imwrite("/datas02/t0323469/hackathon/output_mask/probability_" + str(time.time()).replace(".", "") + ".png", probability_mask)
 out[:, :, 0] = cv2.addWeighted(out[:, :, 0].astype('uint8'), 1.0,
                             cv2.resize((pred * 255).astype('uint8'), img_size).astype('uint8'), 1.0, 0)   
 
 #Save the output images in a directory
-cv2.imwrite("/datas02/t0323469/hackathon/output_image_segmentation/" + str(time.time()).replace(".","") + ".jpg",out)
+cv2.imwrite("test/" + "output_segmentation" + ".jpg", out)
